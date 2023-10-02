b26da5c7bb645810f003c796f942a07b6a55de9e
Sharmin64 <lizaeatsbanana@gmail.com>
2023-10-02 08:34:51 +0600

    style: color changed in the banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -35,13 +35,13 @@ const Banner = () => {
             }}
           >
             <div className="mx-auto">
-              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-indigo-400 via-indigo-400 to-indigo-600 text-transparent bg-clip-text right-1/2">
+              <div className="absolute flex justify-end transform -translate-y-1/2 left-30 top-1/2 bg-gradient-to-r from-[#8db694] via-[#e8f0ea] to-[#e8f0ea] text-transparent bg-clip-text right-1/2">
                 <div className="max-w-md px-5 lg:px-0 ">
                   <h1 className="mb-2 hero-heading lg:text-5xl text-4xl font-bold">
-                    Make Your Summer Unforgettable!
+                    Yoga is the practice of quieting the mind
                   </h1>
                   <Link to="/classes">
-                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-indigo-500 hover:bg-transparent hover:text-indigo-400 border-0 border-b-4 border-indigo-800 cursor-pointer text-white">
+                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-[#a4c5a9] hover:bg-transparent hover:text-[#e8f4ea] border-0 border-b-4 border-[#d1e2d4] cursor-pointer text-white">
                       Enroll Now
                     </button>
                   </Link>
@@ -52,7 +52,7 @@ const Banner = () => {
         </SwiperSlide>
         <SwiperSlide>
           <div
-            className="hero lg:h-[92vh] h-[70vh]"
+            className="hero lg:h-[92vh] h-[70vh] sm:h-auto"
             style={{
               backgroundImage: `url(${bannerB})`,
               backgroundPosition: "center",
@@ -60,13 +60,14 @@ const Banner = () => {
             }}
           >
             <div className="mx-auto">
-              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-indigo-400 via-indigo-400 to-indigo-600 text-transparent bg-clip-text right-1/2">
+              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-[#8db694] via-[#e8f0ea] to-[#e8f0ea] first-line:text-transparent bg-clip-text right-1/2">
                 <div className="max-w-md px-5 lg:px-0 ">
                   <h1 className="mb-2 hero-heading lg:text-5xl text-4xl font-bold">
-                    This Will be the best summer Just For You!
+                    Yoga is the journey of the self, through the self, to the
+                    self
                   </h1>
                   <Link to="/classes">
-                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-indigo-500 hover:bg-transparent hover:text-indigo-400 border-0 border-b-4 border-indigo-800 cursor-pointer text-white">
+                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-[#a4c5a9] hover:bg-transparent hover:text-[#e8f4ea] border-0 border-b-4 border-[#d1e2d4] cursor-pointer text-white">
                       See More
                     </button>
                   </Link>
@@ -85,13 +86,14 @@ const Banner = () => {
             }}
           >
             <div className="mx-auto">
-              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-indigo-400 via-indigo-400 to-indigo-600 text-transparent bg-clip-text right-1/2">
-                <div data-aos="fade-right" className="max-w-md px-5 lg:px-0">
+              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-[#8db694] via-[#e8f0ea] to-[#e8f0ea] text-transparent bg-clip-text right-1/2">
+                <div className="max-w-md px-5 lg:px-0">
                   <h1 className="mb-2 hero-heading lg:text-5xl text-4xl font-bold">
-                    Get Ready For a Lot Of Adventures!
+                    In the heart of summer, take your practice outside, let the
+                    sun kiss your skin
                   </h1>
                   <Link to="/">
-                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl text-white bg-indigo-500 hover:bg-transparent hover:text-indigo-400 border-0 border-b-4 border-indigo-800 cursor-pointer">
+                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl text-white bg-[#a4c5a9] hover:bg-transparent hover:text-[#d2e7d6] border-0 border-b-4 border-[#d1e2d4] cursor-pointer">
                       More About Us
                     </button>
                   </Link>
@@ -110,13 +112,14 @@ const Banner = () => {
             }}
           >
             <div className="mx-auto">
-              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-indigo-400 via-indigo-400 to-indigo-600 text-transparent bg-clip-text right-1/2">
+              <div className="absolute flex justify-end transform -translate-y-1/3 left-30 top-1/2 bg-gradient-to-r from-[#8db694] via-[#e8f0ea] to-[#e8f0ea] text-transparent bg-clip-text right-1/2">
                 <div className="max-w-md px-5 lg:px-0 ">
                   <h1 className="mb-2 hero-heading lg:text-5xl text-4xl font-bold">
-                    So Fun That You Won`t Want To Leave!
+                    Summer vibrant energy mirrors the vitality that yoga brings
+                    to our lives
                   </h1>
                   <Link to="/classes">
-                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-indigo-500 hover:bg-transparent hover:text-indigo-400 border-0 border-b-4 border-indigo-800 cursor-pointer text-white">
+                    <button className="font-medium transition duration-200 shadow-md md:mb-0 px-4 py-2 md:px-8 md:py-3 m-2 text-sm rounded-xl bg-[#a4c5a9] hover:bg-transparent hover:text-[#d2e7d6] border-0 border-b-4 border-[#d1e2d4] cursor-pointer text-white">
                       See More
                     </button>
                   </Link>
